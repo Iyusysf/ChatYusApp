2c5f1025495c341c890ed4a65c731bf11726cfbd
Iyusysf <42061520+Iyusysf@users.noreply.github.com>
2020-07-13 02:20:24 +0700

    lat20 latihan20 live coding

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
 import React from "react";
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Registrasi from "./pages/registrasi";
 import Login from "./pages/login";
-import Lupapassword from "./pages/lupapass";
+import LupaPassword from "./pages/lupapass";
 import Landing from "./pages/landing";
-import Notfound from "./pages/404";
+import NotFound from "./pages/404";
+import Chat from "./pages/private/chat";
+import Pengaturan from "./pages/private/pengaturan";
+import PrivateRoute from "./components/PrivateRoute";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
-import Private from "./pages/private";
-import PrivateRoute from './components/privateRoute';
 import FirebaseProvider from "./components/FirebaseProvider";
-import {ThemeProvider} from '@material-ui/core/styles';
-import Theme from './config/Theme';
-
-
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "./config/theme";
+import { SnackbarProvider } from "notistack";
 export default function App() {
   return (
-<ThemeProvider theme={Theme}>
-<FirebaseProvider>
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Landing} />
-        <Route path="/registrasi" component={Registrasi} />
-        <Route path="/login" component={Login} />
-        <Route path="/lupapass" component={Lupapassword} />
-        
-        <PrivateRoute path="/chat" component={Private}/>
-        <PrivateRoute path="/pengaturan" component={Private}/>
-        
-        <Route component={Notfound} />
-      </Switch>
-    </Router>
-    </FirebaseProvider>
+    <ThemeProvider theme={theme}>
+      <SnackbarProvider maxSnack={3}>
+        <CssBaseline />
+        <FirebaseProvider>
+          <Router>
+            <Switch>
+              <Route path="/" exact component={Landing} />
+              <Route path="/registrasi" component={Registrasi} />
+              <Route path="/login" component={Login} />
+              <Route path="/lupa-password" component={LupaPassword} />
+              <PrivateRoute path="/chat" component={Chat} />
+              <PrivateRoute path="/pengaturan" component={Pengaturan} />
+              <Route component={NotFound} />
+            </Switch>
+          </Router>
+        </FirebaseProvider>
+      </SnackbarProvider>
     </ThemeProvider>
   );
 }
